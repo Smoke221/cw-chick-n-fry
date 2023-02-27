adda9ddd4d39429077729ec41c057397611fea4e
Kancharla Venata Anil Kumar Reddy <114225283+Smoke221@users.noreply.github.com>
2023-02-27 12:07:41 +0530

    Update ProductModel.js

--- Models/ProductModel.js
+++ Models/ProductModel.js
@@ -11,7 +11,7 @@ const AddSchema = mongoose.Schema({
     protein:String,
     userID:String
 })
-schema.set(option, value);
+AddSchema.set(option, value);
 const AddModel = mongoose.model("breakfast",AddSchema)
 
 module.exports = {
